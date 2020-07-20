289e14f35a4d21d9f30d4eccdf5957095665a1ca
Lindsey Hiltner <lindsey@truework.com>
2020-07-20 15:45:13 -0700

    Get husky to run

--- lib/index.ts
+++ lib/index.ts
@@ -1,24 +1,24 @@
-import got from 'got';
-import assert from 'nanoassert';
-import pkg from '../package.json';
-import { VerificationRequest } from './types.js';
+import got from 'got'
+import assert from 'nanoassert'
+import pkg from '../package.json'
+import { VerificationRequest } from './types.js'
 
 const {
   TRUEWORK_API_BASE_URL = 'https://api.truework.com',
   TRUEWORK_API_VERSION = '2019-10-15'
-} = process.env;
+} = process.env
 
 const CANCELLATION_REASONS = [
   'immediate',
   'high-turnaround-time',
   'competitor',
   'wrong-info',
-  'other',
-];
+  'other'
+]
 
 export function truework (config: { token: string }) {
-  assert(typeof config === 'object', 'config object is required');
-  assert(config.token, 'config.token is required');
+  assert(typeof config === 'object', 'config object is required')
+  assert(config.token, 'config.token is required')
 
   const client = got.extend({
     prefixUrl: TRUEWORK_API_BASE_URL,
@@ -28,46 +28,51 @@ export function truework (config: { token: string }) {
       'user-agent': `Truework Node SDK v${pkg.version}; Node ${process.version}`,
       authorization: `Bearer ${config.token}`
     }
-  });
+  })
 
   return {
     verifications: {
       get () {
-        return client.get('verification-requests');
+        return client.get('verification-requests')
       },
       getOne ({ requestId }: { requestId: string }) {
-        assert(requestId, 'requestId is required');
-        return client.get(`verification-requests/${requestId}`);
+        assert(requestId, 'requestId is required')
+        return client.get(`verification-requests/${requestId}`)
       },
       getReport ({ requestId }: { requestId: string }) {
-        assert(requestId, 'requestId is required');
-        return client.get(`verification-requests/${requestId}/report`);
+        assert(requestId, 'requestId is required')
+        return client.get(`verification-requests/${requestId}/report`)
       },
       create (requestObj: VerificationRequest) {
         return client.post({
           url: 'verification-requests',
           method: 'POST',
-          json: requestObj,
-        });
+          json: requestObj
+        })
       },
-      cancel ({ requestId, cancellationReason, cancellationDetails }:
-          {
-            requestId: string;
-            cancellationReason: string;
-            cancellationDetails: string;
-          }
-        ) {
-        assert(requestId, 'requestId is required');
-        assert(cancellationReason in CANCELLATION_REASONS, 'must enter a valid cancellation reason');
+      cancel ({
+        requestId,
+        cancellationReason,
+        cancellationDetails
+      }: {
+        requestId: string
+        cancellationReason: string
+        cancellationDetails: string
+      }) {
+        assert(requestId, 'requestId is required')
+        assert(
+          cancellationReason in CANCELLATION_REASONS,
+          'must enter a valid cancellation reason'
+        )
         return client.patch({
           url: `verification-requests/${requestId}/cancel`,
           method: 'PATCH',
           json: {
             cancellation_reason: cancellationReason,
-            cancellation_details: cancellationDetails,
-          },
-        });
-      },
+            cancellation_details: cancellationDetails
+          }
+        })
+      }
     }
   }
 }
